239f9565f607c35b5a0eacc2af40a86c7d82479a
MayceAlazzawi <maynab4@gmail.com>
2023-02-12 17:02:57 +0300

    adding config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],  theme: {
-    extend: {},
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    // extend: {
+    colors: {
+      mint: "#8DD3BB",
+      darkGreent: "#112211",
+      LightMint: "#CDEAE1",
+      salmon: "#FF8682",
+    },
+    opacity: {
+      36: ".36",
+    },
+    fontWeight: {
+      thin: "100",
+      hairline: "100",
+      extralight: "200",
+      light: "300",
+      normal: "400",
+      medium: "500",
+      semibold: "600",
+      bold: "700",
+      extrabold: "800",
+      "extra-bold": "800",
+      black: "900",
+    },
+    fontFamily: {
+      body: ["Montserrat"],
+    },
+    // },
   },
   plugins: [],
-}
+};
