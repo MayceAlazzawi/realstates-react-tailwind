084f1eeb73e6b1ca4f4789736e44d78f9c6db85a
MayceAlazzawi <maynab4@gmail.com>
2023-02-15 16:56:19 +0300

    creating custom btn component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { FC } from "react";
 import "./index.css";
 import { Button } from "./components/Button";
-import { useRef } from "react";
+import Send from "./assets/icons/send.svg";
 
-function App() {
-  const ref = useRef(null!);
-  const handleClick = () => console.log("CLICKED", ref.current);
-
+const App: FC = () => {
+  const clickHandler = () => {
+    console.log("Hi");
+    return "Hi";
+  };
   return (
     <div className="App text-mint font-body ">
-      <Button onClick={handleClick} ref={ref}>
-        Test
-      </Button>
+      <Button
+        bgBtn={"mint"}
+        textSize={"[14px]"}
+        color={"darkGreen"}
+        fontWeight={"[500]"}
+        width={"84"}
+        m={" m-[3px]"}
+        p={"[16px]"}
+        children={"ابحث"}
+        onClick={clickHandler}
+        icon={Send}
+      />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,45 +1,27 @@
-// import * as React from "react";
+import * as React from "react";
 
-// interface Props {
-//   border: string;
-//   color: string;
-//   children?: React.ReactNode;
-//   height: string;
-//   onClick: () => void;
-//   radius: string;
-//   width: string;
-// }
-// const Button: React.FC<Props> = ({
-//   border,
-//   color,
-//   children,
-//   height,
-//   onClick,
-//   radius,
-//   width,
-// }) => {
-//   return (
-//     <button
-//       onClick={onClick}
-//       className="
+interface Props {
+  bgBtn: string | number;
+  width: string | number;
+  fontWeight: number | string;
+  color: number | string;
+  textSize: number | string;
+  p: number | string;
+  m: number | string;
+  other?: string | number;
+  children: string;
+  onClick: () => string;
+  icon: any;
+}
 
-//       "
-//     >
-//       {children}
-//     </button>
-//   );
-// };
-
-// export default Button;
-
-import { ButtonHTMLAttributes, PropsWithChildren, forwardRef } from "react";
-export const Button = forwardRef<
-  HTMLButtonElement,
-  PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>>
->(function Button(props, ref) {
+export const Button = (props: Props) => {
   return (
-    <button ref={ref} {...props}>
+    <button
+      onClick={props.onClick}
+      className={`flex flex-row  text-center align-center justify-center items-center gap-2 h-[48px] bg-${props.bgBtn} ${props.m}  p-${props.p} w-${props.width} font-${props.fontWeight} text-${props.color} text-${props.textSize} rounded focus:outline-none cursor-pointer`}
+    >
+      <img src={props.icon}></img>
       {props.children}
     </button>
   );
-});
+};
