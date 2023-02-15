2f337626d3e021f85c58c25476a01bc41ae39f7e
MayceAlazzawi <maynab4@gmail.com>
2023-02-16 02:04:01 +0300

    adding navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,10 @@
 import React, { FC } from "react";
 import "./index.css";
-import { Button } from "./components/Button";
-import Send from "./assets/icons/send.svg";
-
+import { Navbar } from "./components/Navbar";
 const App: FC = () => {
-  const clickHandler = () => {
-    console.log("Hi");
-    return "Hi";
-  };
   return (
-    <div className="App text-mint font-body ">
-      <Button
-        bgBtn={"mint"}
-        textSize={"[14px]"}
-        color={"darkGreen"}
-        fontWeight={"[500]"}
-        width={"84"}
-        m={" m-[3px]"}
-        p={"[16px]"}
-        children={"ابحث"}
-        onClick={clickHandler}
-        icon={Send}
-      />
+    <div className="App font-body bg-salmon">
+      <Navbar />
     </div>
   );
 };
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -18,10 +18,30 @@ export const Button = (props: Props) => {
   return (
     <button
       onClick={props.onClick}
-      className={`flex flex-row  text-center align-center justify-center items-center gap-2 h-[48px] bg-${props.bgBtn} ${props.m}  p-${props.p} w-${props.width} font-${props.fontWeight} text-${props.color} text-${props.textSize} rounded focus:outline-none cursor-pointer`}
+      className={`flex flex-row  text-center align-center justify-center items-center gap-2 h-[48px] bg-${props.bgBtn} ${props.m}  p-${props.p} w-${props.width} font-${props.fontWeight} text-${props.color} text-${props.textSize} rounded focus:outline-none cursor-pointer other`}
     >
       <img src={props.icon}></img>
       {props.children}
     </button>
   );
 };
+
+// <Button
+//         bgBtn={"mint"}
+//         textSize={"[14px]"}
+//         color={"darkGreen"}
+//         fontWeight={"[500]"}
+//         width={"84"}
+//         m={" m-[3px]"}
+//         p={"[16px]"}
+//         children={"ابحث"}
+//         onClick={clickHandler}
+//         icon={Send}
+//       />
+
+// import Send from "./assets/icons/send.svg";
+
+// const clickHandler = () => {
+//   console.log("Hi");
+//   return "Hi";
+// };
