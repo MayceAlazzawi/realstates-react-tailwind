14aaf71dc7ff6afcac5d6c09d0e7b90bf3021bea
MayceAlazzawi <maynab4@gmail.com>
2023-02-20 21:04:14 +0300

    addig cards to section2

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import { Hero } from "./components/Hero";
 import { SearchBar } from "./components/searchBar";
 import { Input } from "./components/Input";
 import Data from "./jsonData/Cities.json";
+import { Container } from "./components/section2/Container";
 import { Card } from "./components/section2/Card";
 const App: FC = () => {
   return (
-    <div className="App font-body  flex justify-center  flex-col">
+    <div className="App font-body  flex justify-center items-center  flex-col">
       <Hero />
-      <Card />
+      <Container />
     </div>
   );
 };
--- src/components/section2/card.tsx
+++ src/components/section2/card.tsx
@@ -5,12 +5,12 @@ export interface Props {}
 
 export const Card = (rops: Props) => {
   return (
-    <div className="m-40">
+    <div className="">
       <a
         href="#"
-        className="flex flex-col  justify-end items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row  hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 flex text-right h-[110px] p-[16px] w-max"
+        className="flex flex-col  justify-end items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row  hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 flex text-right h-[90px] p-[6px] max-w-max items-center"
       >
-        <div className="flex flex-col justify-between p-4 leading-normal">
+        <div className="flex flex-col justify-between  leading-normal">
           <h5 className="mb-2 text-[16px] font-semibold tracking-tight text-[#547e6f] dark:text-white">
             اسطنبول، تركيا
           </h5>
@@ -18,10 +18,9 @@ export const Card = (rops: Props) => {
             كوجاتبة . 3+1 . 1000000ليرة
           </p>
         </div>
-        <div className="bg-mint w-1/3 h-[90px] m-2 rounded-lg">
-          {" "}
+        <div className=" w-1/3 h-full m-2 mr-0 rounded-lg ">
           <img
-            className=" w-full rounded-lg h-96 md:h-auto md:w-48  "
+            className=" w-full rounded-lg h-full p-1 pr-0"
             src={Hero0}
             alt=""
           />
